9c821cded9f0d30303688de36a11e110929b0659
leech1211 <leech1211@nate.com>
2024-01-12 02:53:11 +0900

    [구현중] 나선의 추적자 스킬 구현중

--- Assets/Scripts/TurretSkillTriggerBox.cs
+++ Assets/Scripts/TurretSkillTriggerBox.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class vm : MonoBehaviour
+public class TurretSkillTriggerBox : MonoBehaviour
 {
     public GameObject turret;
     // Start is called before the first frame update
