b11b64b55926aede749c01a6624f25f280b2164b
BlueBlahBlah <codydrms417@gmail.com>
2024-01-21 22:40:33 +0900

    스탯 및 증강구현4
    csv 시트에서 랜덤하게 증강 호출 (가중치 미구현)
    
    24.01.21 / 20:40

--- Assets/ReinforceSample/Reinforce Sample/Scripts/Test.cs
+++ Assets/ReinforceSample/Reinforce Sample/Scripts/Test.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+    // Reinforce 컨테이너를 추가하여 (예를 들어 List 형식) 적용된 증강의 넘버를 저장해서
+    // 다음에 증강을 호출할 중복된 증강이 호출되지 않도록 방지하는 기능 필요
+
     [SerializeField]
     private GameObject UI;
     private ReinforceUI reinforceUI;
@@ -34,8 +37,36 @@ public class Test : MonoBehaviour
     private void MixReinforce()
     {
         Debug.Log($"Dialog count : {data_dialog.Count}");
-        reinforceUI.text_left.text = data_dialog[0]["Description"].ToString();
-        reinforceUI.text_middle.text = data_dialog[1]["Description"].ToString();
-        reinforceUI.text_right.text = data_dialog[2]["Description"].ToString();
+
+        int[] numbers = MakeRandomNumbers(0, data_dialog.Count);
+
+        reinforceUI.text_left.text = data_dialog[numbers[0]]["Description"].ToString();
+        reinforceUI.text_middle.text = data_dialog[numbers[1]]["Description"].ToString();
+        reinforceUI.text_right.text = data_dialog[numbers[2]]["Description"].ToString();
+    }
+
+    private static int[] MakeRandomNumbers(int minValue, int maxValue)
+    {
+        List<int> values = new List<int>();
+        for (int v = minValue; v < maxValue; v++)
+        {
+            values.Add(v);
+        }
+
+        int[] result = new int[3];
+        System.Random random = new System.Random();
+        for (int i = 0; i < result.Length; i++)
+        {
+            int randomValue = values[random.Next(0, values.Count)];
+            result[i] = randomValue;
+            Debug.Log($"randomValue = {randomValue}");
+
+            if(!values.Remove(randomValue))
+            {
+                break;
+            }
+        }
+
+        return result;
     }
 }
