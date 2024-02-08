32bc5163b44f1033c14a98bb8b45b0375ce9ad38
ehdgus3130 <kimdong3139@naver.com>
2024-02-08 15:42:26 +0900

    24.02.08
    스턴스킬
    기절기술(속도 감소)
    플레이어 기술 범위(롤 사거리와 비슷한 느낌)

--- Assets/Kim's/Skill/Skill_FireMine/Fire_Mine.cs
+++ Assets/Kim's/Skill/Skill_FireMine/Fire_Mine.cs
@@ -12,6 +12,9 @@ public class Fire_Mine : MonoBehaviour
     private void Destroy() 
     {
         an.StopPlayback();
+        
+        //폭발시 반경안에 존재하는 적들에게 대미지 부가
+
         Destroy(gameObject, 0.45f); 
     }
 
