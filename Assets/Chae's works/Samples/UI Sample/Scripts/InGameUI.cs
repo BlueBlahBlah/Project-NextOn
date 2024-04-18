85a641fba5664240f42fb7b7d3cb392475186a73
BlueBlahBlah <codydrms417@gmail.com>
2024-04-18 10:42:09 +0900

    Dialogue 구현 중2
    문장 넘어갈 때 테스트

--- Assets/Chae's works/Samples/UI Sample/Scripts/InGameUI.cs
+++ Assets/Chae's works/Samples/UI Sample/Scripts/InGameUI.cs
@@ -77,6 +77,7 @@ public class InGameUI : MonoBehaviour
 
     private bool isDialogue; // 대화창 표시 여부
     private List<Dictionary<string, object>> data_Dialogue; // csv 파일 담을 변수
+    public int DialogueNumber;
 
     // Start is called before the first frame update
     void Start()
@@ -90,64 +91,15 @@ public class InGameUI : MonoBehaviour
     void Update()
     {
         UpdatePlayerInfo(); // 플레이어 info 항상 업데이트
-        if (isBoss) UpdateBossInfo(); // 보스 출현 시 보스 info 업데이트
-        if (isGimmick) UpdateGimmickInfo(); // 기믹 출현 시 기믹 info 업데이트
+        UpdateBossInfo(); // 보스 출현 시 보스 info 업데이트
+        UpdateGimmickInfo(); // 기믹 출현 시 기믹 info 업데이트
         UpdateNumOfEnemy(); // 적의 수 업데이트
+        UpdateDialogue(); // 다이얼로그 업데이트
+
 
         FunctionTestUpdate(); // 테스트용 코드
     }
-
-    private void FunctionTestStart() 
-    {
-        // 테스트 초기화
-        PlayerHp = 100f;
-        BossHp = 100f;
-
-        isBoss = true;
-        isGimmick = true;
-
-        PlayerMaxHp = PlayerHp;
-        BossMaxHp = BossHp;
-        GimmickPercent = 0f;
-        NumOfEnemy = 0;
-
-        BossName = "Overflow"; // 현재 출현한 보스 이름 연결
-        GimmickName = "Gimmick"; // 기믹 이름 연결
-    }
     
-    private void FunctionTestUpdate()
-    {
-        // 테스트 업데이트
-        if (PlayerHp == PlayerMaxHp) StartCoroutine("TestCoroutine");
-    }
-
-    IEnumerator TestCoroutine()
-    {
-        // 테스트 코루틴
-        PlayerHp--;
-        BossHp -= 7;
-        NumOfEnemy++;
-        yield return new WaitForSeconds(1f);
-        PlayerHp--;
-        BossHp -= 7;
-        NumOfEnemy++;
-        yield return new WaitForSeconds(1f);
-        UpdateDialogue(0);
-        PlayerHp--;
-        BossHp -= 7;
-        NumOfEnemy++;
-        yield return new WaitForSeconds(1f); 
-        PlayerHp--;
-        BossHp -= 7;
-        NumOfEnemy++;
-        yield return new WaitForSeconds(1f); 
-        PlayerHp--;
-        BossHp -= 7;
-        NumOfEnemy++;
-        yield return new WaitForSeconds(1f);
-
-        yield return null;
-    }
 
     // Update
     #region
@@ -160,47 +112,58 @@ public class InGameUI : MonoBehaviour
 
     public void UpdateBossInfo() // 보스 정보 갱신
     {
-
-        if (!bossInfo.activeInHierarchy)
+        if (isBoss)
         {
-            // 보스가 출현 했지만, UI가 켜지지 않은 상태에서 호출
-            // >> 보스 Info 초기화
-            // 보스의 이름, 이미지 등 업데이트
-            bossName.text = BossName;
-            bossIcon.sprite = Resources.Load($"UI/Image/BossIcons/{BossName}", typeof(Sprite)) as Sprite;
-            bossInfo.SetActive(true);// UI 활성화
+            // 보스 출현 시
+            if (!bossInfo.activeInHierarchy)
+            {
+                // 보스가 출현 했지만, UI가 켜지지 않은 상태에서 호출
+                // >> 보스 Info 초기화
+                // 보스의 이름, 이미지 등 업데이트
+                bossName.text = BossName;
+                bossIcon.sprite = Resources.Load($"UI/Image/BossIcons/{BossName}", typeof(Sprite)) as Sprite;
+                bossInfo.SetActive(true);// UI 활성화
+            }
+            // 보스 정보 갱신
+            bossHp.text = (Mathf.Round(BossHp / BossMaxHp * 100)).ToString() + "%"; // 체력 갱신
+            bossHpBar.fillAmount = BossHp / BossMaxHp; // 체력바 이미지 갱신
         }
-        bossHp.text = (Mathf.Round(BossHp / BossMaxHp * 100)).ToString() + "%"; // 체력 갱신
-        bossHpBar.fillAmount = BossHp / BossMaxHp; // 체력바 이미지 갱신
-
-        if (!isBoss)
+        else
         {
             // 보스 처치 시, 필드 내에 보스가 존재하지 않을 때 
             // >> 보스 Info 비활성화
-            // ***** Update 에서 !isBoss 면 이 함수로 접근 불가능하므로 수정 필요
-            bossInfo.SetActive(false);
+            if (bossInfo.activeInHierarchy) bossInfo.SetActive(false);
         }
     }
 
     public void UpdateGimmickInfo() // 기믹 정보 갱신
     {
-        // UI 활성화
-        if (!gimmickInfo.activeInHierarchy)
+        if (isGimmick)
         {
-            // 기믹이 출현 했지만, UI가 켜지지 않은 상태에서 호출
-            // >> 기믹 Info 초기화
-            // 기믹의 이름, 이미지 등 업데이트
-            //gimmickName.text = GimmickName;
-            //gimmickIcon.sprite = Resources.Load($"UI/Image/GimmickIcons/{GimmickName}", typeof(Sprite)) as Sprite;
+            if (!gimmickInfo.activeInHierarchy)
+            {
+                // 기믹이 출현 했지만, UI가 켜지지 않은 상태에서 호출
+                // >> 기믹 Info 초기화
+                // 기믹의 이름, 이미지 등 업데이트
+                //gimmickName.text = GimmickName;
+                //gimmickIcon.sprite = Resources.Load($"UI/Image/GimmickIcons/{GimmickName}", typeof(Sprite)) as Sprite;
 
-            // 기믹 호출 시 0%로 시작한다고 가정
-            gimmickPercent.text = "0%";
-            gimmickProgressBar.fillAmount = 0f;
+                // 기믹 호출 시 0%로 시작한다고 가정
+                gimmickPercent.text = "0%";
+                gimmickProgressBar.fillAmount = 0f;
 
-            gimmickInfo.SetActive(true);
+                gimmickInfo.SetActive(true);
+            }
+
+            // 기믹의 경우, 기믹의 타입 (시간 기반 / 횟수 기반)에 따라 다르게 코드 작성
+
+        }
+        else
+        {
+            // 기믹 출현 종료
+            if (gimmickInfo.activeInHierarchy) gimmickInfo.SetActive(false);
         }
 
-        // 기믹의 경우, 기믹의 타입 (시간 기반 / 횟수 기반)에 따라 다르게 코드 작성
     }
 
     public void UpdateNumOfEnemy()
@@ -208,35 +171,130 @@ public class InGameUI : MonoBehaviour
         // 몬스터가 출현, 혹은 사망 시 미니맵 하단의 몬스터 수 갱신
         numOfEnemy.text = NumOfEnemy.ToString();
     }
-    #endregion
-    
-    public void UpdateDialogue(int number) // 대화 번호 입력값으로 받음
+
+    public void UpdateDialogue() // 대화 번호 입력값으로 받음
     {
-        if (!dialogue.activeInHierarchy)
+        if (isDialogue)
         {
-            dialogue.SetActive(true);
+            if (!dialogue.activeInHierarchy)
+            {
+                dialogue.SetActive(true);
+            }
+            string Name = data_Dialogue[DialogueNumber]["Character Name"].ToString();
+            
+            // 이미지 변경 << **csv에 저장된 캐릭터 이름은 한글인데, 리소스에서 Load할 이름은 영어여야 함.
+            // 해결 방법
+            // 대화 이미지를 사용할 캐릭터가 많지 않으니 Switch문으로 받아온 Name 비교.
+            // dialogueImage.sprite = Resources.Load($"UI/Image/Characters/{Name}", typeof(Sprite)) as Sprite;
+            
+            // 이름 변경
+            dialogueName.text = Name;
+            
+            // 내용 변경
+            dialogueContent.text = data_Dialogue[DialogueNumber]["Contents"].ToString();
         }
-        string Name = data_Dialogue[number]["Character Name"].ToString();
-        Debug.Log(Name);
-        // 이미지 변경
-        // dialogueImage.sprite = Resources.Load($"UI/Image/Characters/{Name}", typeof(Sprite)) as Sprite;
-        // 이름 변경
-        dialogueName.text = Name;
-        // 내용 변경
-        dialogueContent.text = data_Dialogue[number]["Contents"].ToString();
+        else
+        {
+            if (dialogue.activeInHierarchy) dialogue.SetActive(false);
+        }
     }
+    #endregion
+
+
+
     // Button
     #region
     public void ButtonPause()
     {
+        // 실제 플레이 Pause 기능
 
+        // 다시하기, Setting, 저장 및 종료 UI 호출
     }
 
     public void ButtonPlayerInfo()
     {
+        // 실제 플레이 Pause 기능
 
+        // 플레이어의 현재 정보 (무기, 공격력, 체력 등 스탯 확인?) 열람 가능한 UI 호출
     }
     #endregion
 
-   
+
+    // Test
+    #region
+    private void FunctionTestStart()
+    {
+        // 테스트 초기화
+        PlayerHp = 100f;
+        BossHp = 100f;
+
+        isBoss = true;
+        isGimmick = true;
+        isDialogue = true;
+
+        PlayerMaxHp = PlayerHp;
+        BossMaxHp = BossHp;
+        GimmickPercent = 0f;
+        NumOfEnemy = 0;
+        DialogueNumber = 0;
+
+        BossName = "Overflow"; // 현재 출현한 보스 이름 연결
+        GimmickName = "Gimmick"; // 기믹 이름 연결
+    }
+
+    private void FunctionTestUpdate()
+    {
+        // 테스트 업데이트
+        if (PlayerHp == PlayerMaxHp) StartCoroutine("TestCoroutine");
+    }
+
+    IEnumerator TestCoroutine()
+    {
+        // 테스트 코루틴
+        PlayerHp--;
+        BossHp -= 10;
+        NumOfEnemy++;
+        yield return new WaitForSeconds(1f);
+        PlayerHp--;
+        BossHp -= 10;
+        NumOfEnemy++;
+        yield return new WaitForSeconds(1f);
+        PlayerHp--;
+        BossHp -= 10;
+        NumOfEnemy++;
+        yield return new WaitForSeconds(1f);
+        PlayerHp--;
+        BossHp -= 10;
+        NumOfEnemy++;
+        yield return new WaitForSeconds(1f);
+        PlayerHp--;
+        BossHp -= 10;
+        NumOfEnemy++;
+        yield return new WaitForSeconds(1f);
+        DialogueNumber++;
+        PlayerHp--;
+        BossHp -= 10;
+        NumOfEnemy++;
+        yield return new WaitForSeconds(1f);
+        PlayerHp--;
+        BossHp -= 10;
+        NumOfEnemy++;
+        yield return new WaitForSeconds(1f);
+        PlayerHp--;
+        BossHp -= 10;
+        NumOfEnemy++;
+        yield return new WaitForSeconds(1f);
+        PlayerHp--;
+        BossHp -= 10;
+        NumOfEnemy++;
+        yield return new WaitForSeconds(1f);
+        PlayerHp--;
+        isBoss = false;
+        isGimmick = false;
+        NumOfEnemy++;
+        yield return new WaitForSeconds(1f);
+
+        yield return null;
+    }
+    #endregion
 }
