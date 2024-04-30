1894d4ee951a143009b098514b684a260832e01f
BlueBlahBlah <codydrms417@gmail.com>
2024-04-30 10:31:25 +0900

    다이얼로그, UI매니저 스크립트 생성
    내용물 작성 아직 x
    다이얼로그 기능을 전용 스크립트로 옮기고, UI에 관련된 전반적인 기능을 싱글톤으로 작성되는 UI매니저를 통해 다른 스크립트에서 편하게 접근 가능하도록 하는 방식으로 작성할 예정

---
+++ Assets/Chae's works/Samples/UI Sample/Scripts/UIManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    public InGameUI inGameUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
