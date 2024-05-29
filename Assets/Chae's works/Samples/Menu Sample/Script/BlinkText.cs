2c954bfd9f81dc9d517f7d9fdf5acca4845e30a1
BlueBlahBlah <codydrms417@gmail.com>
2024-05-29 09:46:55 +0900

    0529 변경점
    BlinkText 스크립트가 투명도를 설정할수 있도록 변경

--- Assets/Chae's works/Samples/Menu Sample/Script/BlinkText.cs
+++ Assets/Chae's works/Samples/Menu Sample/Script/BlinkText.cs
@@ -10,6 +10,8 @@ public class BlinkText : MonoBehaviour
     [SerializeField]
     private float initTime = 4f; // 첫 반복 시작 시간
     [SerializeField]
+    private float power = 0f; // 1보다 작은 투명 강도
+    [SerializeField]
     private float invisibleTime = 3f; // 투명화 주기
     [SerializeField]
     private float visibleTime = 1f; // 불투명화 주기
@@ -20,7 +22,7 @@ public class BlinkText : MonoBehaviour
         // TextMeshProUGUI 비활성화
         textMeshPro.color = new Color(textMeshPro.color.r, textMeshPro.color.g, textMeshPro.color.b, 0f);
 
-        // 2초 뒤에 Button 컴포넌트 활성화
+        // initTime 초 뒤에 Button 컴포넌트 활성화
         StartCoroutine("LoopCoroutine");
     }
 
@@ -38,13 +40,11 @@ public class BlinkText : MonoBehaviour
         {
             // 3초 대기 후 TextMeshProUGUI 투명
             yield return new WaitForSeconds(invisibleTime);
-            textMeshPro.color = new Color(textMeshPro.color.r, textMeshPro.color.g, textMeshPro.color.b, 0f);
-            Debug.Log("invisible");
+            textMeshPro.color = new Color(textMeshPro.color.r, textMeshPro.color.g, textMeshPro.color.b, power);
 
             // 1초 대기 후 TextMeshProUGUI 불투명
             yield return new WaitForSeconds(visibleTime);
             textMeshPro.color = new Color(textMeshPro.color.r, textMeshPro.color.g, textMeshPro.color.b, 1f);
-            Debug.Log("visible");
         }
     }
 }
