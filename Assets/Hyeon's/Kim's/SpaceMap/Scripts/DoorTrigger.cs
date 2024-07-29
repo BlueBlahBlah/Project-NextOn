d7815f62da950d0655b20e0f52ddbfd9bf2b537e
ehdgus3130 <kimdong3139@naver.com>
2024-07-29 19:25:52 +0900

    0729
    spacemap 설정 내용 추가
    skill_prob 추가

--- Assets/Hyeon's/Kim's/SpaceMap/Scripts/DoorTrigger.cs
+++ Assets/Hyeon's/Kim's/SpaceMap/Scripts/DoorTrigger.cs
@@ -4,27 +4,26 @@ using UnityEngine;
 
 public class DoorTrigger : MonoBehaviour
 {
-    private Vector3 m_Position;
+    public Vector3 m_Position;
+    public bool m_IsGrounded;
+
+    private GameObject Door;
     private Coroutine m_ReturnCoroutine;
+    private BoxCollider boxCollider;
     private void Start()
     {
-        m_Position = transform.position;
-        Debug.Log("Hello start");
-
+        Door = this.gameObject.transform.GetChild(0).gameObject;
+        boxCollider = this.gameObject.transform.GetComponent<BoxCollider>();
+        m_Position = Door.transform.position;
     }
 
-    private void OnCollisionStay(Collision collision)
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.collider.CompareTag("Player")) Debug.Log("help");
-    }
-    private void OnCollisionEnter(Collision collision)
-    {
-        if (collision.collider.CompareTag("Player"))
+        Debug.Log(other.name);
+        if (other.CompareTag("Player"))
         {
-            if(this.transform.position.y > -5)
-            transform.Translate(Vector3.down * 1);
-            Debug.Log("Hello Keep going");
 
+            StartCoroutine("OpenDoor");
 
             // 충돌 종료 시 위치 복귀를 멈춤
             if (m_ReturnCoroutine != null)
@@ -32,13 +31,12 @@ public class DoorTrigger : MonoBehaviour
                 StopCoroutine(m_ReturnCoroutine);
                 m_ReturnCoroutine = null;
             }
-
         }
     }
 
-    private void OnCollisionExit(Collision collision)
+    private void OnTriggerExit(Collider other)
     {
-        if (collision.collider.CompareTag("Player"))
+        if (other.CompareTag("Player"))
         {
             // 위치 복귀 코루틴 시작
             if (m_ReturnCoroutine == null)
@@ -51,12 +49,23 @@ public class DoorTrigger : MonoBehaviour
     {
         while (transform.position.y < m_Position.y)
         {
-            transform.Translate(Vector3.up * Time.deltaTime); // 매 프레임마다 천천히 이동
+            Door.transform.Translate(Vector3.up * Time.deltaTime); // 매 프레임마다 천천히 이동
             yield return null; // 다음 프레임까지 대기
         }
 
         // 위치가 초기 위치에 도달하면 정확히 초기 위치로 설정
         transform.position = new Vector3(transform.position.x, m_Position.y, transform.position.z);
         m_ReturnCoroutine = null;
+        boxCollider.enabled = true;
+    }
+
+    IEnumerator OpenDoor()
+    {
+        while (transform.position.y > -5) 
+        {
+            Door.transform.Translate(Vector3.down * Time.deltaTime);
+        }
+        boxCollider.enabled = false;
+        yield return null;
     }
 }
