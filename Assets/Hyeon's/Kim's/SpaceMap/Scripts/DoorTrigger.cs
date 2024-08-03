0b383ca6fe9581f1a6f070e567cd9cda8fed389c
ehdgus3130 <kimdong3139@naver.com>
2024-08-03 19:56:07 +0900

    0803
    시나리오 작성 완료및 맵디자인 작업중

--- Assets/Hyeon's/Kim's/SpaceMap/Scripts/DoorTrigger.cs
+++ Assets/Hyeon's/Kim's/SpaceMap/Scripts/DoorTrigger.cs
@@ -9,21 +9,19 @@ public class DoorTrigger : MonoBehaviour
 
     private GameObject Door;
     private Coroutine m_ReturnCoroutine;
-    private BoxCollider boxCollider;
+    private MeshCollider[] ColliderS;
     private void Start()
     {
-        Door = this.gameObject.transform.GetChild(0).gameObject;
-        boxCollider = this.gameObject.transform.GetComponent<BoxCollider>();
+        Door = gameObject.transform.GetChild(0).gameObject;
+        ColliderS = gameObject.GetComponents<MeshCollider>();
         m_Position = Door.transform.position;
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        Debug.Log(other.name);
         if (other.CompareTag("Player"))
         {
-
-            StartCoroutine("OpenDoor");
+            StartCoroutine(OpenDoor());
 
             // 충돌 종료 시 위치 복귀를 멈춤
             if (m_ReturnCoroutine != null)
@@ -45,9 +43,9 @@ public class DoorTrigger : MonoBehaviour
             }
         }
     }
-    private IEnumerator ReturnToPosition()
+    IEnumerator ReturnToPosition()
     {
-        while (transform.position.y < m_Position.y)
+        while (Door.transform.position.y < m_Position.y)
         {
             Door.transform.Translate(Vector3.up * Time.deltaTime); // 매 프레임마다 천천히 이동
             yield return null; // 다음 프레임까지 대기
@@ -56,16 +54,23 @@ public class DoorTrigger : MonoBehaviour
         // 위치가 초기 위치에 도달하면 정확히 초기 위치로 설정
         transform.position = new Vector3(transform.position.x, m_Position.y, transform.position.z);
         m_ReturnCoroutine = null;
-        boxCollider.enabled = true;
+        EnableColliders(true);
     }
 
     IEnumerator OpenDoor()
     {
-        while (transform.position.y > -5) 
+        while (Door.transform.position.y > -5) 
         {
-            Door.transform.Translate(Vector3.down * Time.deltaTime);
+            Door.transform.Translate(Vector3.down * Time.deltaTime * 1.5f);
+            EnableColliders(false);
+            yield return null;
         }
-        boxCollider.enabled = false;
-        yield return null;
+    }
+    private void EnableColliders(bool enabled)
+    {
+        foreach (var collider in ColliderS)
+        {
+            collider.enabled = enabled;
+        }
     }
 }
---
+++ Assets/Hyeon's/Kim's/SpaceMap/Scripts/ProbDestroy.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProbDestroy : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (!collision.gameObject.CompareTag("Untagged"))
+        {
+            Debug.Log(collision.gameObject.tag);
+            // 충돌이 발생하면 3초 뒤에 오브젝트를 삭제
+            StartCoroutine(DestroyAfterDelay());
+        }
+
+    }
+
+    private IEnumerator DestroyAfterDelay()
+    {
+        // 3초 대기
+        yield return new WaitForSeconds(3);
+
+        // 오브젝트 삭제
+        Destroy(gameObject);
+    }
+}
