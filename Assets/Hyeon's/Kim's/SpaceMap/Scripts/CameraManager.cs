e1484d5368ee6190ba5b3f16e4980260129172bf
ehdgus3130 <kimdong3139@naver.com>
2024-08-14 01:04:11 +0900

    0814
    맵 추가 작업
    캐릭터 매니저 작업
    카메라 오류 정위치 완료

--- Assets/Hyeon's/Kim's/SpaceMap/Scripts/CameraManager.cs
+++ Assets/Hyeon's/Kim's/SpaceMap/Scripts/CameraManager.cs
@@ -6,15 +6,33 @@ public class CameraManager : MonoBehaviour
 {
     public GameObject Player;
     public Transform[] targetPos;   //카메라의 위치들을 담는 배열
+    public Camera FreeLookCam;
+    public Camera TopCam;
 
     public int currentRoomIndex = -1;  //현재 방의 인덱스
-    
+    public bool isTopview;
+    void Start()
+    {
+        FreeLookCam = GetComponent<Camera>();
+        TopCam = GetComponent<Camera>();
+        isTopview = true;
+    }
     void Update()
     {
-        transform.LookAt(Player.transform);
+        if (isTopview)
+        {
+            TopCam.enabled = true;
+            FreeLookCam.enabled = false;
+        }
+        else
+        {
+            TopCam.enabled = false;
+            FreeLookCam.enabled = true;
+            transform.LookAt(Player.transform);
+        }
     }
 
-
+    public void ChaingePointOfView() => isTopview = !isTopview;
     public void SetTarget(int idx)
     {
         this.transform.position = targetPos[idx].position;
