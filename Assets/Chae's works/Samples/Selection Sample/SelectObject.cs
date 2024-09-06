b6280f2fb0c2a64252385b7762d1be3ab44fd55a
BlueBlahBlah <109202758+BlueBlahBlah@users.noreply.github.com>
2024-09-06 20:30:06 +0900

    Selection

---
+++ Assets/Chae's works/Samples/Selection Sample/SelectObject.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectObject : MonoBehaviour
+{
+    [SerializeField]
+    private string sceneName;
+
+    [SerializeField]
+    private GameObject selectCanvas;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+
+        }
+    }
+}
