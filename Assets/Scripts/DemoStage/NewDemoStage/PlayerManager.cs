90c8ea84e895d71b3ad162bff9e2e0eeed34e411
leech1211 <leech1211@nate.com>
2024-10-09 15:00:58 +0900

    [수정] PlayerManager 싱글톤 제거

--- Assets/Scripts/DemoStage/NewDemoStage/PlayerManager.cs
+++ Assets/Scripts/DemoStage/NewDemoStage/PlayerManager.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class PlayerManager : MonoBehaviour
@@ -50,10 +51,7 @@ public class PlayerManager : MonoBehaviour
     
     private void Awake()
     {
-        TotalHealth = 100;  //시작 시 체력 100
-        Health = TotalHealth;
-        SkillCoolTimeRate = 0;     //시작 시 스킬 쿨타임
-        Death = false;
+        
         if (null == instance)
         {
             //이 클래스 인스턴스가 탄생했을 때 전역변수 instance에 게임매니저 인스턴스가 담겨있지 않다면, 자신을 넣어준다.
@@ -62,14 +60,14 @@ public class PlayerManager : MonoBehaviour
             //씬 전환이 되더라도 파괴되지 않게 한다.
             //gameObject만으로도 이 스크립트가 컴포넌트로서 붙어있는 Hierarchy상의 게임오브젝트라는 뜻이지만, 
             //나는 헷갈림 방지를 위해 this를 붙여주기도 한다.
-            DontDestroyOnLoad(this.gameObject);
+            //DontDestroyOnLoad(this.gameObject);
         }
         else
         {
             //만약 씬 이동이 되었는데 그 씬에도 Hierarchy에 GameMgr이 존재할 수도 있다.
             //그럴 경우엔 이전 씬에서 사용하던 인스턴스를 계속 사용해주는 경우가 많은 것 같다.
             //그래서 이미 전역변수인 instance에 인스턴스가 존재한다면 자신(새로운 씬의 GameMgr)을 삭제해준다.
-            Destroy(this.gameObject);
+            //Destroy(this.gameObject);
         }
     }
     //게임 매니저 인스턴스에 접근할 수 있는 프로퍼티. static이므로 다른 클래스에서 맘껏 호출할 수 있다.
@@ -91,6 +89,57 @@ public class PlayerManager : MonoBehaviour
         player_LongWeapon = GameObject.Find("Check_Sprite_Long");
         player_NonWeapon = GameObject.Find("Check_Sprite");
         player_CloseWeapon = GameObject.Find("Check_Sprite_Short");
+        
+        // player_NonWeapon과 같은 깊이의(같은 부모를 가진) 오브젝트를 찾음
+        Transform parentTransform = player_NonWeapon.transform.parent;
+        foreach (Transform sibling in parentTransform)
+        {
+            DropItemPosition dropItemPosition = sibling.GetComponent<DropItemPosition>();
+    
+            if (dropItemPosition != null)
+            {
+                // DropItemPosition을 가진 오브젝트를 찾았을 때 _dropItemPosition에 연결
+                _dropItemPosition = dropItemPosition;
+                break;
+            }
+        }
+        
+        player_LongWeapon.SetActive(false);
+        player_CloseWeapon.SetActive(false);
+        
+        TotalHealth = 100;  //시작 시 체력 100
+        Health = TotalHealth;
+        SkillCoolTimeRate = 0;     //시작 시 스킬 쿨타임
+        Death = false;
+    }
+    
+    /*void OnEnable()
+    {
+        // 씬 매니저의 sceneLoaded에 체인을 건다.
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+    
+    void OnDisable()
+    {
+        SceneManager.sceneLoaded -= OnSceneLoaded;
+    }*/
+    
+    // 체인을 걸어서 이 함수는 매 씬마다 호출된다.
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        init();
+    }
+
+    private void init()
+    {
+        TotalHealth = 100;  //시작 시 체력 100
+        Health = TotalHealth;
+        SkillCoolTimeRate = 0;     //시작 시 스킬 쿨타임
+        Death = false;
+        /*player_LongWeapon = GameObject.Find("Check_Sprite_Long");
+        player_NonWeapon = GameObject.Find("Check_Sprite");
+        player_CloseWeapon = GameObject.Find("Check_Sprite_Short");*/
+        Debug.Log("********************tlqkf*******************");
 
         // 각 무기 리스트에 추가할 오브젝트 이름
         string[] longWeaponNames = {
@@ -111,9 +160,21 @@ public class PlayerManager : MonoBehaviour
         };
 
         // parent 오브젝트 찾기
-        player_LongWeapon = GameObject.Find("Check_Sprite_Long");
-        player_NonWeapon = GameObject.Find("Check_Sprite");
-        player_CloseWeapon = GameObject.Find("Check_Sprite_Short");
+        if (GameObject.Find("Check_Sprite_Long") is not null)
+        {
+            player_LongWeapon = GameObject.Find("Check_Sprite_Long");
+        }
+        if (GameObject.Find("Check_Sprite") is not null)
+        {
+            player_NonWeapon = GameObject.Find("Check_Sprite");
+        }
+        if (GameObject.Find("Check_Sprite_Short") is not null)
+        {
+            player_CloseWeapon = GameObject.Find("Check_Sprite_Short");
+        }
+        
+       
+        
 
         // 모든 자식 오브젝트들 가져오기
         Transform[] longWeaponChildren = player_LongWeapon.GetComponentsInChildren<Transform>(true);
@@ -180,6 +241,10 @@ public class PlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (player_NonWeapon is null)
+        {
+            init();
+        }
         if (Health <= 0)        //체력이 다 닳은 경우
         {
             Health = 0;         //체력바가 길어지는 것을 방지
@@ -199,7 +264,6 @@ public class PlayerManager : MonoBehaviour
             
             
         }
-        
         //현재 총기류를 먹은경우
         if (player_LongWeapon.activeSelf)
         {
