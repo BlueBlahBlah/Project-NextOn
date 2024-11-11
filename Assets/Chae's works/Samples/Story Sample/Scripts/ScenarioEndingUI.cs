10f5470ae934f2611ae04a8abf6dff5fb273b489
BlueBlahBlah <109202758+BlueBlahBlah@users.noreply.github.com>
2024-11-11 18:12:28 +0900

    1111 1812

--- Assets/Chae's works/Samples/Story Sample/Scripts/ScenarioEndingUI.cs
+++ Assets/Chae's works/Samples/Story Sample/Scripts/ScenarioEndingUI.cs
@@ -1,12 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ScenarioEndingUI : MonoBehaviour
 {
+    [Header("UI Object")]
     [SerializeField]
     Image BlackFadeInOut;
+    [SerializeField]
+    private Image backgroundComputer;
+    [SerializeField]
+    private TextMeshProUGUI endText1;
+    [SerializeField]
+    private GameObject endText2; // 이 TMP는 Blink가 있기 때문에 활성화 시키도록
+    [SerializeField]
+    private Button endButton;
 
     private VolumeController volumeController;
 
@@ -25,12 +36,28 @@ public class ScenarioEndingUI : MonoBehaviour
                 volumeController = volumeControllerObject.AddComponent<VolumeController>();
             }
             Invoke("FadeOut", 3f);
+
+            //Test
+            Invoke("ChangeImage", 5f);
         }
     }
 
     public void ChangeImage()
     {
+        // Lerp 필요
+        backgroundComputer.color = new Color(1, 1, 1, 1);
+        endText1.color = new Color(1, 1, 1, 1);
+        Invoke("ActivateEndText", 1f);
+    }
 
+    public void ActivateEndText()
+    {
+        endText2.SetActive(true);
+    }
+
+    public void ActivateButton()
+    {
+        endButton.interactable = true;
     }
 
     public void Fadein()
